08d3fdb4426e13a05980bb74521d4ad4a42a86c8
Dipali Samgir <samgirdipali40@gmail.com>
2025-01-29 14:57:07 +0530

    Update todo.js

--- modules/todo.js
+++ modules/todo.js
@@ -1,1 +1,20 @@
-a
+let mongoose = require('mongoose');
+
+let todoSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    task: {
+     type:String,
+    required: true,
+    }
+    
+})
+let Todo = mongoose.model('todo', todoSchema) 
+module.exports = Todo
+
