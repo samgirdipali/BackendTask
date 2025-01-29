dd2fb55fdef15badeced6bfce4de5dfe61eb655f
Dipali Samgir <samgirdipali40@gmail.com>
2025-01-29 14:56:30 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,1 +1,21 @@
-a
+let auth = passport.authenticate('local',{session:false})
+
+passport.use(new localstatergy(async(USERNAME, password, done)=>{
+  try{
+      console.log('received request', USERNAME, password);
+    let user =  await Todo.findOne({username:USERNAME});
+    if(!user){
+      return done(null, false,{message: 'User not found'})
+    }
+    let match =  user.password === password? true: false;
+    // let match = await user.comparePassword(password);
+    if(match){
+      return done(null, user)
+    }else{
+      return done(null, false, {message: 'Password is incorrect'})
+    }
+  }
+  catch(err){
+    console.log(err);
+  }
+}))
